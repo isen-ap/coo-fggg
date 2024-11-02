9fafe2741dc6cb094e9478158362bc0bdf3d2af8
Gabriel Gressier <75422446+GabGr25@users.noreply.github.com>
2024-11-02 15:31:26 +0100

    feat: alignments

--- src/interface/api/rest/character_controller.ts
+++ src/interface/api/rest/character_controller.ts
@@ -1,16 +1,21 @@
 import { Request, Response, Application } from "express";
 import { ClassesService } from "../../../application/services/classes_service";
+import { AlignmentService } from "../../../application/services/alignment_service";
 class CharacterController {
   classesService: ClassesService;
+  alignmentService: AlignmentService;
 
-  constructor(app: Application, classesService: ClassesService) {
+  constructor(app: Application, classesService: ClassesService, alignmentService: AlignmentService) {
     this.classesService = classesService;
-    app.get("/characters-info", this.getCharacters.bind(this));
+    this.alignmentService = alignmentService;
+    app.get("/character", this.getCharacters.bind(this));
   }
 
   async getCharacters(_req: Request, res: Response) {
     const classes = await this.classesService.getClasses();
-    res.json(classes);
+    const alignments = await this.alignmentService.getAlignments();
+    console.log(alignments);
+    res.json({ classes, alignments });
   }
 }
 
