661e06d6be1da79c162078907bdc8565f7838b16
Clément Gambier <78157383+clement-gambier2@users.noreply.github.com>
2024-11-07 10:05:40 +0100

    fix

--- src/domain/entities/skill.ts
+++ src/domain/entities/skill.ts
@@ -1,11 +1,9 @@
 export class Skill {
-  id : String;
-  name : String;
-  description: String;
+  id: String;
+  name: String;
 
-  constructor(id: String, name: String, description: String) {
+  constructor(id: String, name: String) {
     this.id = id;
     this.name = name;
-    this.description = description;
   }
 }
